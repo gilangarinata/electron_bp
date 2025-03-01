639e5de88bf2d8f8cd563b2af569b8212498b551
gilangarinata <gilangarinata@gmail.com>
2025-03-02 00:04:30 +0700

    a

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 	output: 'export',
 	trailingSlash: true,
 	distDir: 'build',
